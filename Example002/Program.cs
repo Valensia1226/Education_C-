d9b8b2a0b6c373d92ef118d7654c849826fb16a8
Valensia1226 <valensia16@yandex.ru>
2023-02-08 19:45:38 +0300

    Second programm

---
+++ Example002/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите ваше имя "); //вывод текста в терминал
+string username = Console.ReadLine(); //запрос информации от пользователя
+Console.Write("Привет, ");
+Console.Write(username);
