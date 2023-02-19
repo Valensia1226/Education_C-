aaf77d1e8c442c949fea4fc4b860586fa707ff88
Valensia1226 <valensia16@yandex.ru>
2023-02-19 17:42:15 +0300

    HomeWork3 is done

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,90 @@
+﻿/*Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет; 12821 -> да; 23432 -> да*/
+
+Console.WriteLine("Задача 19");
+int Digits(int _a) //количество символов в числе
+{
+    int count = 0;
+    while(_a != 0)
+    {
+        _a /= 10;
+        count ++;
+    }
+    return count;
+}
+
+int number;
+Console.Write($"Введите пятизначное число: ");
+    while (!int.TryParse(Console.ReadLine(), out number)) //проверка на ввод числа, а не буквы
+    {
+        Console.WriteLine("Ошибка ввода! Введите пятизначное число: ");
+    }
+
+int length = Digits(number);
+if (length != 5) //проверка на количество символов в числе
+{
+    Console.WriteLine("Ошибка ввода! Введите пятизначное число: ");
+    int.TryParse(Console.ReadLine()!, out number);
+}
+
+char[] c = number.ToString().ToCharArray();
+int[]mass = new int[c.Length];
+
+for (int i = 0; i < mass.Length; i++)
+            mass[i] = c[i] - '0';
+
+if (mass[0] == mass[4])
+{
+    if (mass[1]==mass[3])
+        if (mass[2]==mass[2]) Console.WriteLine("Является палиндромом");
+}
+else Console.WriteLine("Не является палиндромом");
+Console.WriteLine();
+
+/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84; A (7,-5, 0); B (1,-1,9) -> 11.53*/
+Console.WriteLine("Задача 21");
+void TakeCoords(string A)
+{
+    Console.WriteLine($"Введите координаты точки {A}");
+}
+int TakeCor(string N)
+    {
+        int _X;
+        Console.Write($"Введите {N}: ");
+        int.TryParse(Console.ReadLine()!, out _X);
+        return _X;
+    }
+
+TakeCoords("A");
+int xA = TakeCor("X");
+int yA = TakeCor("Y");
+int zA = TakeCor("Z");
+
+TakeCoords("B");
+int xB = TakeCor("X");
+int yB = TakeCor("Y");
+int zB = TakeCor("Z");
+
+double Distance = Math.Sqrt((xB-xA)*(xB-xA)+(yB-yA)*(yB-yA)+(zB-zA)*(zB-zA));
+Console.WriteLine(Math.Round(Distance, 2));
+Console.WriteLine();
+
+/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27; 5 -> 1, 8, 27, 64, 125*/
+Console.WriteLine("Задача 23");
+
+int num;
+Console.Write($"Введите число: ");
+while (!int.TryParse(Console.ReadLine(), out num))
+{
+    Console.WriteLine("Ошибка ввода! Введите число: ");
+}
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i*i*i} ");    
+}
--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -1,2 +1,70 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Семинар 4. Функции.
+int str;
+int TakeInt(string _str = "Введите число: ")
+{
+    Console.Write($"Введите число: ");
+    int.TryParse(Console.ReadLine()!, out str);
+    return str;
+}
+
+/*Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out b);*/
+
+/*int d = 2;
+int f = 7;
+Summ(d, f);
+Summ(11, 9);
+
+void Summ(int _a, int _b)
+{
+    int c = _a + _b;
+    Console.WriteLine($"{_a} + {_b} = {c}");
+}*/
+
+//На входе число, на выходе сумма от 1 до этого числа.
+
+/*int a = TakeInt();
+int sum = 0;
+for (int i = 0; i <= a; i++)
+    sum +=i;
+Console.Write($"{a} -> {sum}");*/
+
+//Принимает на вход число, выдает количество цифр в числе
+/*int a = TakeInt();
+Console.Write($"{a} -> {Digits(a)}");
+
+int Digits(int _a)
+{
+    int count = 0;
+    while(_a != 0)
+    {
+        _a /= 10;
+        count ++;
+    }
+    return count;
+}*/
+
+//Структуры и классы
+struct Point
+{
+    public double x;
+    public double y;
+    public double z;
+    void TakeCoords()
+    {
+        string coords = Console.ReadLine()!;
+        double[] numbers = coords.Split(' ').Where(x => double.TryParse(x, out _)).Select(double.Pars).ToArray();
+        /*List<int> numbers = new List<int>(Array.ConvertAll(coords.Split(' '), int.Parse)); //динамический список, который может увеличивать или уменьшать свой размер в ходе выполнения программы
+        //Split разделяет строку, int.Pars переводит в int и записывает в массив*/
+        if (numbers.Length != 3)
+        {
+            x = 0; y = 0; z = 0;
+            return;
+        }
+        x = numbers[0];
+        y = numbers[1];
+        z = numbers[2];
+    }
+}
