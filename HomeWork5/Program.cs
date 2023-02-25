30e08dd51eb75d752b8bebfe8d662f2f83412234
Valensia1226 <valensia16@yandex.ru>
2023-02-25 17:11:08 +0300

    HomeWork5 is done

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -1,2 +1,97 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int size = WriteCol();
+
+int[] array = FillArray(size, 99, 999);
+Print(array);
+int Col = 0;
+
+foreach(int el in array)
+    if (el % 2 == 0) Col++;
+
+Console.WriteLine($"Количество четных чисел в массиве = {Col}");
+Console.WriteLine();
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19; [-4, -6, 89, 6] -> 0
+
+int size2 = WriteCol();
+int[] mass = FillArray(size2, -100, 100);
+Print(mass);
+int Sum = 0;
+
+for (int i = 1; i < mass.Length; i++)
+    if (i % 2 != 0) Sum = Sum + mass[i];
+
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях в массиве = {Sum}");
+Console.WriteLine();
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+int size3 = WriteCol();
+Console.WriteLine("До скольки округлить?");
+int round;
+int.TryParse(Console.ReadLine(), out round);
+
+double[] arr = FillArrayDouble(size3, 0, 100, round);
+Print(arr);
+
+SelectionSort(arr);
+Print(arr);
+double rez = arr[0] - arr[arr.Length - 1];
+
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {Math.Round(rez, round)}");
+Console.WriteLine();
+
+//Функции
+int[] FillArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    for(int i=0; i < size; i++)
+        arr[i] = new Random().Next(min, max);
+    return arr;
+}
+
+double[] FillArrayDouble(int size, int min, int max, int _round)
+{
+    double[] arr = new double[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(min, max) + new Random().NextDouble();
+        arr[i] = Math.Round(arr[i], _round);
+    }
+    return arr;
+}
+
+void Print(Array arr)
+{
+    for(int i=0; i < arr.Length; i++)
+        Console.Write($"{arr.GetValue(i)} ");
+    Console.WriteLine();
+}
+
+int WriteCol()
+{
+    Console.WriteLine("Сколько чисел в массиве?");
+    int _size;
+    int.TryParse(Console.ReadLine(), out _size);
+    return _size;
+}
+
+void SelectionSort(double[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[minPosition]) minPosition = j;
+        }
+        double temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;        
+    }
+}
--- Seminar5/Program.cs
+++ Seminar5/Program.cs
@@ -1,6 +1,6 @@
 ﻿//Задать массив из 12 эл-ов, случайные числа [-9, 9]. Найти сумму отрицательных и положительных элементов массива.
 
-int[] array = FillArray(12, -9, 10);
+/*int[] array = FillArray(12, -9, 10);
 
 Console.Write($"{String.Join(", ", array)}\n"); //берет все элементы массива, преобразует их в строку и разбивает все элементы запятой
 //фактически заменяет собой прописанный нами метод Print
@@ -15,22 +15,24 @@ foreach(int el in array) //работает быстрее for, так как н
 {
     pos += el > 0 ? el : 0; //тернарный оператор: условие, вопрос, значение при верном условии, двоеточие, значение при неверном условии
     neg += el < 0 ? el : 0;
-    //Console.Write(el); //обращается к элементу
-    /*if (el > 0)
+    //или
+    int temp = el > 0 ? pos += el : neg += el;
+    //или
+    if (el > 0)
         pos += el;
     else
-        neg += el;*/
+        neg += el;
 }
 
- /*for(int i=0; i < array.Length; i++)
+ for(int i=0; i < array.Length; i++)
  {
     if (array[i] > 0)
         pos += array[i];
     else
         neg += array[i];
- }*/
+ }
 
-Console.Write($"pos sum = {pos} \nneg sum = {neg}");
+Console.Write($"pos sum = {pos} \nneg sum = {neg}");*/
 
 
 int[] FillArray(int size, int min=0, int max=10)
@@ -48,4 +50,47 @@ void Print(Array arr)
     Console.WriteLine();
 }
 
-//
+//Меняем элементы массива на противоположные по знаку
+/*int a = 5;
+a = ChangeA(a);
+Console.Write(a);
+
+int[] array = FillArray(12, -9, 10);
+Console.Write($"{String.Join(", ", array)}\n");
+Console.Write($"{String.Join(", ", InverseElements(array))}\n");
+
+int ChangeA(int _a)
+{
+    _a *= -1;
+    return _a;
+}
+
+int[] InverseElements(int[] arr) //будет работать и в виде void так как для изменения массива не нужно его переприсваивать, функция сработает и без return
+{
+    for (int i=0; i < arr.Length; i++)
+        arr[i] *= -1;
+    return arr;
+}*/
+
+//Произведение пар в массиве (первый элемент на последний, второй на предпоследний и т.д.)
+int[] array = FillArray(5, 0, 10);
+int[] pairs = new int[array.Length / 2 + array.Length % 2];
+
+Console.Write($"{String.Join(", ", array)}\n");
+MultiplyPairs(array, pairs);
+Console.Write($"{String.Join(", ", pairs)}\n");
+
+
+void MultiplyPairs(int[] arr, int[] pair)
+{
+    int size = arr.Length;
+    int pairSize = arr.Length / 2 + arr.Length % 2;
+
+    for (int i=0; i < size/2; i++)
+    {
+        int rev = size - i - 1;
+        pair[i] = arr[i] * arr[rev];
+    }
+    if (size % 2 != 0)
+        pair[pairSize -1] = arr[pairSize];
+}
